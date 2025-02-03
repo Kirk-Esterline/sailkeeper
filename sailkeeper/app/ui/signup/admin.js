4f47e4b0f2e7f00dd700173026f9544cb92dbe3d
Kirk Esterline <kirk.esterline@gmail.com>
2025-02-03 17:02:43 -0500

    added basic framework for adding the admin data

---
+++ sailkeeper/app/ui/signup/admin.js
@@ -0,0 +1,31 @@
+import { addAdmin } from "@/app/models/data";
+
+export default function AddAdmin () {
+
+    async function addAdminData(formData) {
+        'use server'
+        const newData = [formData.get('userName'), formData.get('adminEmail'), formData.get('role')]
+        await addAdmin(newData)
+    }
+
+    return (
+        <>
+        <div className="md:w-2/5 bg-slate-500 mx-10 my-10 px-10 py-10 md:inline-block">
+            <h2 className="text-navBackground text-center">Sign Up An Administrator</h2>
+            <form action={addAdminData} >
+                <div className="flex">
+                    <section className="w-3/6">
+                        <h3>Admin Information</h3>
+                        <input name="userName" type="text" placeholder="Admin Name" className="block py-3 my-1" required></input>
+                        <input name="adminEmail" type="text" placeholder="Admin Email" className="block py-3 my1" required></input>
+                        <input name="role" type="text" placeholder="Company Role" className="block py-3 my-1" required></input>
+                    </section>
+                </div>
+            <button type="submit" className="bg-sky-100 rounded-lg text-slate-950 w-40">Create</button>
+            </form>
+        </div>
+        </>
+
+    )
+
+}
