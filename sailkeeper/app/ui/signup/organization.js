1e057ce8621ca30b91381bab1172772350695ab0
Kirk Esterline <kirk.esterline@gmail.com>
2025-02-04 16:45:11 -0500

    Laying the ground work for adding next-auth. Creating a new branch from this point and will attempt to get auth working.

--- sailkeeper/app/ui/signup/organization.js
+++ sailkeeper/app/ui/signup/organization.js
@@ -23,12 +23,13 @@ export default async function SignUpOrganization() {
                         <input name="admin" type="string" placeholder="Admin Name" className="block py-3 my-1" required></input> 
                         <input name="joinID" type="string" placeholder="Unique Business Name" className="block py-3 my-1" required></input>
                     </section>
-                    <section className="w-3/6">
+                    {/* // Removed as part of testing
+                     <section className="w-3/6">
                         <h3>Admin Information</h3>
                         <input name="userName" type="text" placeholder="Admin Name" className="block py-3 my-1" required></input>
                         <input name="adminEmail" type="text" placeholder="Admin Email" className="block py-3 my1" required></input>
                         <input name="role" type="text" placeholder="Company Role" className="block py-3 my-1" required></input>
-                    </section>
+                    </section> */}
                 </div>
             <button type="submit" className="bg-sky-100 rounded-lg text-slate-950 w-40">Create</button>
             </form>
