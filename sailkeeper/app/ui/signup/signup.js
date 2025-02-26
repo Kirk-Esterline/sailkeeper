ed388ab06e0d61034c356118b007f3ef9a95468b
Kirk Esterline <kirk.esterline@gmail.com>
2025-02-26 16:51:02 -0500

    Changed wording on User sign up from

--- sailkeeper/app/ui/signup/signup.js
+++ sailkeeper/app/ui/signup/signup.js
@@ -15,7 +15,7 @@ export default function SignUpForm() {
         <form action={formAction} className="space-y-3">
             <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                 <h1 className="mb-3 text-2xl">
-                    Please sign up to continue.
+                    Sign up to continue.
                 </h1>
                 <div className="w-full">
                     <div>
