f9f89bedb4c09cf35c6e088ce5c16ff02e3514ad
Kirk Esterline <kirk.esterline@gmail.com>
2025-03-04 16:50:10 -0500

    working on toggling the signup forms visibility so that the forms alternate rather than stack.

--- sailkeeper/app/signup/page.js
+++ sailkeeper/app/signup/page.js
@@ -7,30 +7,27 @@ import SignUpForm from "../ui/signup/signup";
 
 export default function signUpNewOrganization() {
 
-    const [showUserForm, setShowUserForm] = useState(false);
-    const [showOrgForm, setShowOrgForm] = useState(false)
+    const [showForms, setShowForms] = useState("")
 
     return (
         <main>
             <section className="flex justify-center pt-10">
                 <div className="md:pr-5">
                 <BigButton onClick={() => {
-                    console.log(showOrgForm)
-                    setShowOrgForm(!showOrgForm);
+                    setShowForms("Organizations");
                 }} buttonText="Sign Up an Organization" href=""/>
                 </div>
                 <div className="md:pr-5">
                 <BigButton onClick={() => {
-                    console.log(!setShowUserForm)
-                    setShowUserForm(!showUserForm)
+                    setShowForms("User")
                 }} buttonText="Sign Up a new User" href=""/>
                 </div>
             </section>
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:mt-10">
-                <div className={`${!showUserForm ? "hidden" : ''}`}>
+                <div className={`${showForms === "User" ? "hidden" : ''}`}>
                     <SignUpForm />
                 </div>
-                <div className={`${!showOrgForm ? "hidden" : ''}`}>
+                <div className={`${showForms === "Organizations" ? "hidden" : ''}`}>
                     <SignUpOrganization />   
                 </div>
 
@@ -42,3 +39,7 @@ export default function signUpNewOrganization() {
     )
 }
 
+
+
+// const [showUserForm, setShowUserForm] = useState(false);
+    // const [showOrgForm, setShowOrgForm] = useState(false)
